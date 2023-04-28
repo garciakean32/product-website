617d05704028e8407171e517e167a01242a78844
Kean Valgere E. Garcia <110815853+garciakean32@users.noreply.github.com>
2023-04-28 05:57:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function show() {
+    document.getElementById('box1').style.display='none';;
+    document.getElementById('box2').style.display='flex';
+    document.getElementById('box3').style.display='flex';;
+}
